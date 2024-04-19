517a51c4e36fb052af7fb934f74943a6bd60a4ac
TroyDeca <noahfrillerhoj@gmail.com>
2024-04-19 10:39:14 +0200

    imofeife

--- Banan Spil/Assets/Player.cs
+++ Banan Spil/Assets/Player.cs
@@ -2,44 +2,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
-    private RaycastHit2D hit;
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 velocity;
+
 
     private void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+
+    }
+
+    private void Update()
+    {
+        velocity = Vector2.zero;
+        velocity = velocity.normalized * Time.deltaTime;
+        velocity.x = Input.GetAxisRaw("Horizontal");
+        velocity.y = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
     {
-
-        moveDelta = Vector3.zero;
-
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        moveDelta = new Vector3(x, y, 0);
-
-        if (moveDelta.x > 0)
-        {
-            transform.localScale = Vector3.one;
-        }
-        else if (moveDelta.x < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Humans", "Blocking"));
-        if (hit.collider == null)
-        {
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
-        }
-
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.x), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Humans", "Blocking"));
-        if (hit.collider == null)
-        {
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
-        }
+        rb.MovePosition(rb.position + velocity * moveSpeed * Time.fixedDeltaTime);
     }
+
 }
