fe92561959201c5c06d85262e3859ee26e558065
TroyDeca <noahfrillerhoj@gmail.com>
2024-04-26 10:12:50 +0200

    Walking animations done

--- Banan Spil/Assets/Player.cs
+++ Banan Spil/Assets/Player.cs
@@ -3,28 +3,62 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    public Animator animator;
 
-    Vector2 velocity;
+    public float moveSpeed;
 
+    private Rigidbody2D rb;
+
+    private float x;
+    private float y;
+
+    private Vector2 input;
+    private bool moving;
 
     private void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        velocity = Vector2.zero;
-        velocity = velocity.normalized * Time.deltaTime;
-        velocity.x = Input.GetAxisRaw("Horizontal");
-        velocity.y = Input.GetAxisRaw("Vertical");
+        GetInput();
+        Animate();
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + velocity * moveSpeed * Time.fixedDeltaTime);
+        rb.velocity = input * moveSpeed;
+    }
+
+    private void GetInput()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        input = new Vector2(x, y);
+        input.Normalize();
+    }
+
+    private void Animate()
+    {
+
+        if (input.magnitude > 0.1f || input.magnitude < -0.1f)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+
+        if (moving)
+        {
+            animator.SetFloat("X", x);
+            animator.SetFloat("Y", y);
+        }
+
+        animator.SetBool("Moving", moving);
     }
 
 }
